6c901e1301ec5bf95a80b79140977ce25f788f41
Michael Kroon <kroonology@gmail.com>
2019-09-10 15:30:29 -0600

    add Flashcard and Card components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Container, Header } from 'semantic-ui-react';
+import FlashcardForm from './components/FlashcardForm';
+import Card from './components/Card';
 // import './App.css';
 
 class App extends React.Component {
+  state = {
+    flashcards: [
+      {id: 1, question: 'What is you name?', answer: 'Michael'},
+      {id: 2, question: 'What are you studying?', answer: 'ReactJS'},
+      {id: 3, question: 'From where are you learning it?', answer: 'DevPoint Labs'}
+    ]
+  }
   render() {
     return (
       <Container>
         <Header as='h1'>Hello World!</Header>
+        <FlashcardForm></FlashcardForm>
+        <Card></Card>
       </Container>
     )
   }
---
+++ src/components/FlashcardForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class FlashcardForm extends React.Component {
+  render() {
+    return (
+      <div>This is coming from FlashcardForm</div>
+    )
+  }
+}
+
+export default FlashcardForm;
